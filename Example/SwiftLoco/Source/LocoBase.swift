0464ef8be6d0f85855c0c0400d4d5a785f46b591
Eugene Yagrushkin <eugene@castingworkbook.com>
2017-04-13 16:27:35 -0700

    a little bit more

--- Example/SwiftLoco/Source/LocoBase.swift
+++ Example/SwiftLoco/Source/LocoBase.swift
@@ -1,16 +1,20 @@
 //
-//  Created by Eugene Yagrushkin on 2017-04-13
+//  LocoBase.swift
+//  SwiftLoco
+//
+//  Created by Eugene Yagrushkin on 2017-04-13.
+//  Copyright © 2017 LocoEye. All rights reserved.
 //
 
 import Foundation
 
 extension String {
-    // NSLocalizedString shorthand
 
     public var localized: String {
         return localized()
     }
     
+    // NSLocalizedString shorthand
     fileprivate func localized(comment: String = "")->String{
         return NSLocalizedString(self, comment: "")
     }
